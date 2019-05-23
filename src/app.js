5a31c7454bc1670b0e495a1f45f9c09a3c1da1d6
Eduardo Rosas <lalorosas@outlook.com>
2019-05-23 17:03:50 -0500

    Separate in components

--- src/app.js
+++ src/app.js
@@ -1,49 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const ExpenseDashboardPage = () => (
-    <div>
-        Dashboard component
-    </div>
-);
-
-const AddExpensePage = () => (
-    <div>
-        Add expense
-    </div>
-);
-
-const EditExpensePage = () => (
-    <div>
-        Edit expense
-    </div>
-);
-
-const HelpPage = () => (
-    <div>
-        Help
-    </div>
-);
-
-const NotFoundPage = () => (
-    <div>
-        404 - Not found!
-    </div>
-);
-
-const routes = (
-    <BrowserRouter>
-        <Switch>
-            <Route path='/' component={ExpenseDashboardPage} exact={true}/>
-            <Route path='/create' component={AddExpensePage}/>
-            <Route path='/edit' component={EditExpensePage}/>
-            <Route path='/help' component={HelpPage}/>
-            <Route component={NotFoundPage}/>
-        </Switch>
-    </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter/>, document.getElementById('app'));
