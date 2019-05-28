34ec556fc85ce8250ac790fbf07c98998ea54105
Eduardo Rosas <lalorosas@outlook.com>
2019-05-28 10:30:49 -0500

    Create expense form and get values

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ExpenseForm from './ExpenseForm';
 
 const AddExpensePage = () => (
     <div>
-        Add expense
+        <h2>Add Expense</h2>
+        <ExpenseForm/>
     </div>
 );
 
